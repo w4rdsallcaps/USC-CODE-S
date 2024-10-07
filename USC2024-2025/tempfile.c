8c7e9782e39623923a51d6f4c890044b704b525f
w4rds <kentbusinessemail@gmail.com>
2024-10-07 10:49:22 +0800

    rawr

--- USC2024-2025/tempfile.c
+++ USC2024-2025/tempfile.c
@@ -18,34 +18,37 @@ int main(){
     for(j=1; j<= spaces; j++){
         printf(" ");
     }
+    
     printf("Future Value \n");
 
-    for(i=1; i <= 6; i++){
-        
+    for(i=1; i <= 6; i++){    
     printf("%d",i);
     for(j=1; j <= spaces + 3; j++){
             printf(" ");
         }
+    
     printf("%.2f",principleMoney = principleMoney + 200 );
     printf("\n");
     sleep(1);
     }
+    
     printf("\n");
     printf("Paying debt");
     for(i = 1; i <= 3; i++){
         printf(".");
         sleep(1);
     }
+    
     printf("\n");
     
-    printf("%-15s %-25s\n", "Month", "Remaining Debt Balance");
+    printf("%-15s %-20s\n", "Month", "Remaining Debt Balance");
 
     i = 0;
-    while(debtBalance > 0.1 && i < 12){
+    while(debtBalance > 0.1 && i < 12)
+    {
         printf("%-15s",months[i]);
         i++;
         debtBalance = debtBalance - 2500;
-
         if (debtBalance < 0){
             debtBalance = 0;
         }
